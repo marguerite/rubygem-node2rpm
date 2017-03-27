6d0bbfa4713833134ea2f9d12335d72ce927c165
marguerite <i@marguerite.su>
2017-03-27 21:25:24 +0800

    refactor jsonobject

--- lib/node2rpm/jsonobject.rb
+++ lib/node2rpm/jsonobject.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 module Node2RPM
   class JSONObject
     def initialize(list)
@@ -5,41 +7,26 @@ module Node2RPM
     end
 
     def parse(list = @list, json = [])
-      jsonobject = Struct.new(:name,
-                              :version,
-                              :parent,
-                              :parentversion,
-                              :dependencies)
       list.each do |k, v|
-        if v[:dependencies].empty?
-          json << jsonobject.new(k,
-                                 v[:version],
-                                 v[:parent],
-                                 v[:parentversion],
-                                 nil)
-        else
-          json << jsonobject.new(k,
-                                 v[:version],
-                                 v[:parent],
-                                 v[:parentversion],
-                                 v[:dependencies].keys)
-          parse(v[:dependencies], json)
-        end
+        obj = OpenStruct.new
+        obj.name = k
+        obj.version = v[:version]
+        obj.parent = v[:parent]
+        obj.parentversion = v[:parentversion]
+        obj.dependencies = v[:dependencies].empty? ? nil : v[:dependencies].keys
+        json << obj
+        parse(v[:dependencies], json) unless obj.dependencies.nil?
       end
       json
     end
 
     def include?(pkg, version)
-      result = false
       json = parse
       json.each do |j|
         # the name@version should be considered as include.
-        if j.name =~ /^#{pkg}(@\d.*)?$/ && j.version == version
-          result = true
-          break
-        end
+        return true if j.name =~ /^#{pkg}(@\d.*)?$/ && j.version == version
       end
-      result
+      return false
     end
   end
 end
