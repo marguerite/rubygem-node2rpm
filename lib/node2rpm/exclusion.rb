b20174aa74898f8b55fb82d82e674ec222b9faed
marguerite <i@marguerite.su>
2017-03-28 19:52:45 +0800

    fix exclusion, should handle nil

--- lib/node2rpm/exclusion.rb
+++ lib/node2rpm/exclusion.rb
@@ -3,11 +3,11 @@ require 'node_semver'
 module Node2RPM
   class Exclusion
     def initialize(pkgs)
-      raise "the parameter can't be nil!" if pkgs.nil?
       @pkgs = pkgs
     end
 
     def exclude?(pkg, version = nil)
+      return false if @pkgs.nil?
       @pkgs.each do |k, v|
         if version.nil?
           return true if k == pkg
